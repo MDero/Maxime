c0060fb15a6308492f0da08b1a2c778edec36de2
MDero <maxime.derobillard@gmail.com>
2014-12-04 09:41:23 +0100

    Premier commit de base avec la classe Lapin

--- src/maxime/Maxime.java
+++ src/maxime/Maxime.java
@@ -15,7 +15,7 @@ public class Maxime {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+          
         
         System.out.println("Hello wordl");
         Lapin lap = new Lapin("bob", 5);
