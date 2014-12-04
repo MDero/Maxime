e855c1a03deb36517c43073c26c604187e6dc530
MDero <maxime.derobillard@gmail.com>
2014-12-04 09:36:41 +0100

    Premier commit de base avec la classe Lapin

---
+++ src/maxime/Lapin.java
@@ -0,0 +1,45 @@
+package maxime;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Maxime
+ */
+public class Lapin {
+    private String name;
+    private int age;
+
+    public Lapin(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public void crier() {
+        System.out.println("HAAAAAAAAA");
+        
+    }
+    
+    
+}
